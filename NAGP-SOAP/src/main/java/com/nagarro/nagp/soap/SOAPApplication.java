bd56f0cbb6e76422fb6ec9c97da797e811e33021
dineshkumar03 <dinesh.kumar03@nagarro.com>
2020-01-21 17:51:25 +0530

    SOAP Service

---
+++ NAGP-SOAP/src/main/java/com/nagarro/nagp/soap/SOAPApplication.java
@@ -0,0 +1,12 @@
+package com.nagarro.nagp.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SOAPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SOAPApplication.class, args);
+	}
+}
